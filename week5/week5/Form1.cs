cfcdacc9ebd4b6599face636d253e946ea32dae9
Ádám Benes <adam.benes@stud.uni-corvinus.hu>
2023-03-28 08:46:17 +0200

    Webszolgáltatás hívása

--- week5/week5/Form1.cs
+++ week5/week5/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week5.MnbServiceReference;
 
 namespace week5
 {
@@ -15,6 +16,23 @@ namespace week5
         public Form1()
         {
             InitializeComponent();
+            GetRates();
+        }
+
+        private void GetRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
