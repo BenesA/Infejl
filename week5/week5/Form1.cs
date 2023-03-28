9799ffd2c9bd5c1c7ce45ed3ea968c3aabe069b4
Ádám Benes <adam.benes@stud.uni-corvinus.hu>
2023-03-28 09:29:47 +0200

    Diagram

--- week5/week5/Form1.cs
+++ week5/week5/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using week5.Entites;
 using week5.MnbServiceReference;
@@ -24,6 +25,26 @@ namespace week5
 
             var r = GetRates();
             GetXmlData(r);
+            PrintChart();
+        }
+
+        private void PrintChart()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
         private string GetRates()
@@ -55,6 +76,7 @@ namespace week5
 
                 var rate = (XmlElement)item.ChildNodes[0];
                 var currency = rate.GetAttribute("curr");
+                var unit = int.Parse(rate.GetAttribute("unit")); //Így is lehet parse-olni
 
                 var value = rate.InnerText;
                 
