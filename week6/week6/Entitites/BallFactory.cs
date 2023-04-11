948503b4e764f7d4ed9e071a18cd8e9ff1700788
Ádám Benes <adam.benes@stud.uni-corvinus.hu>
2023-04-11 09:16:50 +0200

    Absztrakció gyárban

--- week6/week6/Entitites/BallFactory.cs
+++ week6/week6/Entitites/BallFactory.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using week6.Abstractions;
 
 namespace week6.Entitites
 {
-    public class BallFactory
+    public class BallFactory : IToyFactory
     {
-        public Ball CreateNew()
+        public Toy CreateNew()
         {
             return new Ball();
         }
